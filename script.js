e01a939517f650d5489896f38e57c588587ac6b1
unknown <wirawanaryo99@gmail.com>
2026-01-13 11:55:39 +0700

    basic features (main objective done)

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 let canvasSize = prompt("How big is the canvas?");
 const canvas = document.querySelector(".canvas")
 
+const eraser = document.getElementById("eraser");
+let eraserStatus = false; 
+eraser.addEventListener("click", function(){
+  if(eraserStatus === false){
+     eraser.textContent = "Paint Mode";
+     eraserStatus = true
+  } else{
+     eraser.textContent = "Eraser Mode"
+     eraserStatus = false;
+  }
+});
+
+
 function createPixel(canvasSize){
   pixelSize = 700/canvasSize
+  
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
   pixel.style.height = `${pixelSize}px`;
   pixel.style.flexBasis = `${pixelSize}px`;
-  return pixel
-}
 
+  pixel.addEventListener("mouseover", function() {
+    if (eraserStatus) {
+      pixel.style.backgroundColor = "white";
+    } else{
+      pixel.style.backgroundColor = "black";
+    }      
+  });
+  
+  return pixel
+};
 
 function appendPixels(canvasSize){ 
   for(let i =0; i < canvasSize*canvasSize; i++){ 
-    const pixel = createPixel(canvasSize);
+    const pixel = createPixel(canvasSize);    
     canvas.appendChild(pixel);
-  }  
-}
+  } ; 
+};
+
 appendPixels(canvasSize)
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", function(){  
+  const pixels = document.querySelectorAll(".pixel");
+  pixels.forEach(pixel => {
+    pixel.style.backgroundColor = "white";
+  });
+});
